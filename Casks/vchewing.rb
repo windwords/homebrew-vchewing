be37e4f0cbc2e99f4ebf310d468cf9461d55c82e
風語 <windwords001@gmail.com>
2022-08-02 16:33:08 +0800

    ADD: README.md and Casks/vchewing.rb

---
+++ Casks/vchewing.rb
@@ -0,0 +1,11 @@
+cask "vchewing" do
+  version "1.8.6"
+  sha256 "14ab356bf8451bb4160273c69963757c82a1072d2f9ec58b792afbc885a2e239"
+  
+  url "https://github.com/vChewing/vChewing-macOS/releases/download/#{version}/vChewing-macOS-#{version}-unsigned.pkg"
+  name "vChewing"
+  desc "Phonabet-based input methods for typing Mandarin Chinese."
+  homepage "https://vchewing.github.io/"
+
+  pkg "vChewing-macOS-#{version}-unsigned.pkg", allow_untrusted: true
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# homebrew-vchewing
+
+## Description
+
+homebrew-vchewing is a Homebrew Cask to install [vChewing-macOS](https://github.com/vChewing/vChewing-macOS). 
+
+## Usage
+
+```shell
+brew tap windwords/vchewing
+brew install --cask vchewing
+```
+
+## License
+
+Licensed under the [GNU AGPL](https://raw.githubusercontent.com/windwords/homebrew-vchewing/master/LICENSE.txt).
