ba334e6995c3f6eafbbd8f671a5d3092d6ea5d7d
風語 <windwords001@gmail.com>
2022-10-04 07:23:30 +0800

    UPDATE to vChewing 2.8.5 SP2

--- Casks/vchewing.rb
+++ Casks/vchewing.rb
@@ -1,8 +1,8 @@
 cask "vchewing" do
   version "2.8.5"
-  sha256 "134d7ae42ecccc0abd11d65fbec4d34f330926df25c9e1102ea9f846d2148d0e"
+  sha256 "68fca9bebbbbd78e0fb57511c61a9fb6c7605338ae99dc9dee4879530f1d3a69"
   
-  url "https://github.com/vChewing/vChewing-macOS/releases/download/#{version}/vChewing-macOS-#{version}-unsigned.pkg"
+  url "https://github.com/vChewing/vChewing-macOS/releases/download/#{version}/vChewing-macOS-#{version}-signed.pkg"
   name "vChewing"
   desc "Phonabet-based input methods for typing Mandarin Chinese."
   homepage "https://vchewing.github.io/"
