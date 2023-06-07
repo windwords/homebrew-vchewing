26cb5b47f166674dd5185ee136cda9f766f54621
ShikiSuen <shikisuen@outlook.com>
2023-06-07 15:25:02 +0800

    3.4.9 Notarized PKG.

--- Casks/vchewing.rb
+++ Casks/vchewing.rb
@@ -1,6 +1,6 @@
 cask "vchewing" do
   version "3.4.9"
-  sha256 "27ba825a3e7a98902934922032e91d9c5eece5bdfccb8f42010b24df2780c28f"
+  sha256 "58ba1f332b62466ec54a1f8b1138ae64a4c0cfc6658b6a6a1a818999f8473d16"
   
   url "https://gitee.com/vChewing/vChewing-macOS/releases/download/#{version}/vChewing-macOS-#{version}-signed.pkg"
   name "vChewing"
